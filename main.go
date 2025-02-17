770a98f3b5a55f2f1147a98130ff16c7e1063307
yonatana <yonatana@jfrog.com>
2025-02-17 17:08:29 +0200

    Pushed secrets

--- main.go
+++ main.go
@@ -12,6 +12,9 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	token:= "0d6a9bb5af126f73350a2afc058492765446aaad"
+	key := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL2V4YW1wbGUuYXV0aDAuY29tLyIsImF1ZCI6Imh0dHBzOi8vYXBpLmV4YW1wbGUuY29tL2NhbGFuZGFyL3YxLyIsInN1YiI6InVzcl8xMjMiLCJpYXQiOjE0NTg3ODU3OTYsImV4cCI6MTQ1ODg3MjE5Nn0.CA7eaHjIHz5NxeIJoFK9krqaeZrPLwmMmgI_XiQiIkQ"
+	url := "https://myuser:" + token + "@api.github.com"
 	res, err := edlib.StringsSimilarity("string1", "string2", edlib.Levenshtein)
 	if err != nil {
 		fmt.Println(err)
